e2523de6a5487d537bd81bfd235f9ad76f0586ad
TG-SNOWFLAKE-TRAINING <108324479+TG-SNOWFLAKE-TRAINING@users.noreply.github.com>
2022-06-28 10:15:47 +0200

    Update streamlit_app.py
    Set Index hinzugefügt

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,16 @@ streamlit.text('🥑🍞 Avocado Toast')
 #Smoothies
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#Datei einlesen
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
+#Index setzen sodass Fruit Namen ausgewählt werden statt Zahlen
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#Tabelle anzeigen
+streamlit.dataframe(my_fruit_list)
+
+
