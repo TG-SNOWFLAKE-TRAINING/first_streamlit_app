4608b0f5398eabea6e3ddc663aebf92fa38ffd22
TG-SNOWFLAKE-TRAINING <108324479+TG-SNOWFLAKE-TRAINING@users.noreply.github.com>
2022-07-07 10:26:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Tabelle anzeigen
 streamlit.dataframe(fruits_to_show)
 
+#####
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+#streamlit.text(fruityvice_response.json())
 
 # json normalizing 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
