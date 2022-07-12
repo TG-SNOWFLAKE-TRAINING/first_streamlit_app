898650daca5bd8fc2ff0fc8a3413ed11d53e8213
TG-SNOWFLAKE-TRAINING <108324479+TG-SNOWFLAKE-TRAINING@users.noreply.github.com>
2022-07-12 14:52:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,10 @@ except URLError as e:
 #SNWOFLAKE Connection
 
 streamlit.header("The fruit load list contains:")
+
 #Snowflake related functions
-def get_fruit_load_liste():
+
+def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
      my_cur.execute("select * from fruit_load_list")
      return my_cur.fetchall()
@@ -64,11 +66,11 @@ def get_fruit_load_liste():
 # Add a button to load the fruit
 if streamlit.button('get Fruit Load List'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-     my_data_rows = get_fruit_load_liste()
+     my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 
 #StopPoint
 streamlit.stop()
